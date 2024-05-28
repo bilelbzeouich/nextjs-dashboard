1a70600afe49be7d324ba8fd505de96aec2775da
bilelbzeouich <bzeouichbilel6@gmail.com>
2024-05-29 00:54:47 +0100

    red

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -117,6 +117,7 @@ export async function authenticate(
 ) {
   try {
       await signIn('credentials', formData);
+        revalidatePath('/dashboard');
       redirect('/dashboard');
   } catch (error) {
     if (error instanceof AuthError) {
